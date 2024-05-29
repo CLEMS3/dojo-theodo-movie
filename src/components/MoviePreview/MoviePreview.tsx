e5764db11f6425bb0ca282bb53dbd7c444e9673b
null <null>
2024-05-29 20:07:40 +0200

    NIveau rookie

--- src/components/MoviePreview/MoviePreview.tsx
+++ src/components/MoviePreview/MoviePreview.tsx
@@ -1,11 +1,31 @@
+import { POSTER_URL_PREFIX } from "../../App";
+import { Movie } from "../../models";
 import styles from "./MoviePreview.module.css";
 
-const MoviePreview = () => {
+const MoviePreview = ({movie} : {movie : Movie}) => {
   const goToMovieDetails = () => {
     // A DEFINIR EXPERT
   };
 
-  return <div className={styles.movieCard} onClick={goToMovieDetails}></div>;
+  var gradeOnFive = movie.vote_average/2;
+
+  
+
+  return <div className={styles.movieCard} onClick={goToMovieDetails}>
+
+    <div>
+    <img src={POSTER_URL_PREFIX + movie.poster_path} crossOrigin=""/>
+    <p>{movie.title}</p>
+    <p>
+      {[...Array(5)].map((_, index) => (
+        <span className={`${styles.starItem} ${
+          index <= gradeOnFive-1 ? styles.green : styles.red
+        }`}>★</span>
+      ))}
+    </p>
+
+  </div>
+  </div>;
 };
 
 export default MoviePreview;
